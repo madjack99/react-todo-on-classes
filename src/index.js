c2407053f6482a1ab166d0aae097f1ee78d65391
madjack <kavanokami@gmail.com>
2020-01-18 20:48:24 +0400

    feat: add separate components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = <h1>Hello World</h1>;
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn</li>
+      <li>Build</li>
+    </ul>
+  );
+};
 
-ReactDOM.render(el, document.getElementById('root'));
+const AppHeader = () => {
+  return <h1>My todo list</h1>;
+};
+
+const SearchPanel = () => {
+  const searchStyle = {
+    fontSize: '25px',
+  };
+
+  return <input style={searchStyle} type="text" placeholder="search" />;
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
