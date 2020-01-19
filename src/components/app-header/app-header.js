835c3043a55af252a010a192f25a6d11b1d50741
madjack <kavanokami@gmail.com>
2020-01-19 17:49:33 +0400

    feat: add class components

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './app-header.css';
 
-const AppHeader = () => {
+const AppHeader = ({ toDo, done }) => {
   return (
     <div className="app-header d-flex">
       <h1>Todo list</h1>
-      <h2>1 more to do, 4 done</h2>
+      <h2>
+        {toDo} more to do, {done} done
+      </h2>
     </div>
   );
 };
