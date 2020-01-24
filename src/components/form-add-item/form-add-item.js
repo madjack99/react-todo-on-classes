5c9dcc1284e76ec4a6a8edb70a2c6bee82b3093c
madjack <kavanokami@gmail.com>
2020-01-24 10:49:10 +0400

    feat: add form to add new items

--- src/components/form-add-item/form-add-item.js
+++ src/components/form-add-item/form-add-item.js
@@ -3,17 +3,32 @@ import React, { Component } from 'react';
 import './form-add-item.css';
 
 export default class FormAddItem extends Component {
+  state = {
+    label: '',
+  };
+
+  onLabelChange = e => {
+    this.setState({
+      label: e.target.value,
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+  };
+
   render() {
-    const { onItemAdded } = this.props;
     return (
-      <div className="form-add-item">
-        <button
-          onClick={() => onItemAdded('hello world')}
-          className="btn btn-outline-secondary my-btn"
-        >
-          Add Item
-        </button>
-      </div>
+      <form className="form-add-item" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control my-input"
+          onChange={this.onLabelChange}
+          placeholder="What needs to be done?"
+        />
+        <button className="btn btn-outline-secondary my-btn">Add Item</button>
+      </form>
     );
   }
 }
