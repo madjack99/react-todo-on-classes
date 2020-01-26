a664f670c9aafd2ca4751c13de7bed8793d96df7
madjack <kavanokami@gmail.com>
2020-01-26 14:01:32 +0400

    feat: add keywords filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -26,6 +26,7 @@ export default class App extends Component {
       this.createTodoItem('Make Awesome App'),
       this.createTodoItem('Have a lunch'),
     ],
+    term: '',
   };
 
   deleteItem = id => {
@@ -78,21 +79,37 @@ export default class App extends Component {
     });
   };
 
+  search(items, term) {
+    if (term.length === 0) return items;
+
+    return items.filter(item => {
+      return item.label.toLowerCase().includes(term.toLowerCase());
+    });
+  }
+
+  onSearchChange = term => {
+    this.setState({
+      term,
+    });
+  };
+
   render() {
-    const { todoData } = this.state;
+    const { todoData, term } = this.state;
     const doneCount = todoData.filter(el => el.done).length;
     const todoCount = todoData.length - doneCount;
+
+    const visibleItems = this.search(todoData, term);
 
     return (
       <div className="todo-app">
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
-          <SearchPanel />
+          <SearchPanel onSearchChange={this.onSearchChange} />
           <ItemStatusFilter />
         </div>
 
         <TodoList
-          todos={this.state.todoData}
+          todos={visibleItems}
           onDeleted={this.deleteItem}
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}
